241e3de7d5e8b00d666bb9080dcac5acae92c889
Edem Alloh <timreaperdnb@aol.com>
2016-05-23 02:06:23 +0100

    more responsive fixes for entry page layout

--- dist/app/js/main.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){router.load_page()});var router=new function(){this.get_pathname=function(){return window.location.pathname.split("/")},this.load_page=function(){var t=router.get_pathname();"index.html"==t[t.length-1]&&$("#page").html(portfolio.master)}};
